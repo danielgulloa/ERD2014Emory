dbcd915785df8bdba4d6c65030d77893a787a197
DenisSavenkov <denis.savenkov@emory.edu>
2014-04-14 23:48:25 -0400

    Adding freebase lexicon collector hadoop project

--- emory-erd-tools/freebase-lexicon-builder/src/main/java/edu/emory/erd/tools/LexiconBuilder.java
+++ emory-erd-tools/freebase-lexicon-builder/src/main/java/edu/emory/erd/tools/LexiconBuilder.java
@@ -1,7 +1,12 @@
 package edu.emory.erd.tools;
 
 import java.io.IOException;
-import java.util.*;
+import java.lang.Integer;
+import java.lang.String;
+import java.lang.StringBuilder;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
@@ -11,31 +16,37 @@ import org.apache.hadoop.util.*;
 import org.apache.hadoop.mapred.JobConf;
 
 /**
- *
+ * Builds freebase entities lexicon from CLueWeb documents annotations.
  */
 public class LexiconBuilder {
 
-    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+    public static class LexiconBuilderMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-            String line = value.toString();
-            StringTokenizer tokenizer = new StringTokenizer(line);
-            while (tokenizer.hasMoreTokens()) {
-                word.set(tokenizer.nextToken());
-                output.collect(word, one);
-            }
+        public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+            String[] fields = value.toString().split("\t");
+            // Output Freebase mid and phrase
+            output.collect(new Text(fields[7]), new Text(fields[2]));
         }
     }
 
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+    public static class LexiconBuilderReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
             int sum = 0;
+            java.util.Map<String, Integer> count = new HashMap<String, Integer>();
             while (values.hasNext()) {
-                sum += values.next().get();
+                String phrase = values.next().toString();
+                if (count.containsKey(phrase))
+                    count.put(phrase, count.get(phrase) + 1);
+                else
+                    count.put(phrase, 1);
             }
-            output.collect(key, new IntWritable(sum));
+            StringBuilder phrases = new StringBuilder();
+            for (java.util.Map.Entry<String, Integer> entry : count.entrySet()) {
+                phrases.append(entry.getKey() + "\t" + entry.getValue() + "\t");
+            }
+            output.collect(new Text(key), new Text(phrases.toString()));
         }
     }
 
@@ -46,9 +57,9 @@ public class LexiconBuilder {
         conf.setOutputKeyClass(Text.class);
         conf.setOutputValueClass(IntWritable.class);
 
-        conf.setMapperClass(Map.class);
-        conf.setCombinerClass(Reduce.class);
-        conf.setReducerClass(Reduce.class);
+        conf.setMapperClass(LexiconBuilderMapper.class);
+        conf.setCombinerClass(LexiconBuilderReducer.class);
+        conf.setReducerClass(LexiconBuilderReducer.class);
 
         conf.setInputFormat(TextInputFormat.class);
         conf.setOutputFormat(TextOutputFormat.class);
